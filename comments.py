5f34b1013b49a46f55bd0426754e15d931b7719b
Cornelius Kasokola <theecornelius@gmail.com>
2026-08-17 23:40:31 +0200

    Constants && Comments

---
+++ comments.py
@@ -0,0 +1,33 @@
+#Single line comment
+#This variable saves customer name
+customer_name = "Guido Van Rossum"
+print("Customer Name: ", customer_name)
+
+
+'''
+This is a multi line comment 
+It has mutiple statements 
+Use for longer statement
+
+Below i am saving customer product quamtity
+'''
+blue_trouser_quantity = 2
+
+#Single line comment used to comment out piece of code
+#x = 2
+
+'''
+Commenting out 
+Lines of code 
+'''
+
+'''
+x = 10
+y = 30
+result = x + y
+print("Sum result: ", result)
+'''
+
+
+z = x + y
+print("Sum result: ", z)
---
+++ constants.py
@@ -0,0 +1,9 @@
+#Constant - Immutable variables
+isVariable = 100
+print("Variable value: ", isVariable)
+
+SCHOOL_TUITION_CONSTANT = 3500
+print("Constant value: ", SCHOOL_TUITION_CONSTANT)
+
+SCHOOL_TUITION_CONSTANT = 10000
+print("Constant new value: ", SCHOOL_TUITION_CONSTANT)
